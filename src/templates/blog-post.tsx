75f948be2c60bcb4eb0ee2374df680158976eaed
couturecraigj <couturecraigj@gmail.com>
2020-06-12 15:51:41 -0500

    BlogPost Template fix

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,32 +1,32 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
+
 import Layout from "../components/Layout"
+import Image from "../components/Image"
 import SEO from "../components/Seo"
 
-const IndexPage = ({ data }) => {
+const IndexPage = ({data}) => {
+  console.log(data)
   return (
-    <Layout>
-      <SEO title={data.file.childMarkdownRemark.frontmatter.title} />
-      <h1>{data.file.childMarkdownRemark.frontmatter.title}</h1>
-      <div
-        dangerouslySetInnerHTML={{ __html: data.file.childMarkdownRemark.html }}
-      />
-    </Layout>
-  )
-}
+  <Layout>
+    <SEO title={data.file.childMarkdownRemark.frontmatter.title} />
+  <h1>{data.file.childMarkdownRemark.frontmatter.title}</h1>
+    <div dangerouslySetInnerHTML={{__html: data.file.childMarkdownRemark.html}} />
+  </Layout>
+)}
 
 export default IndexPage
 
 export const query = graphql`
-  query BlogPostTemplateQuery($slug: String!) {
-    file(childMarkdownRemark: { frontmatter: { slug: { eq: $slug } } }) {
-      childMarkdownRemark {
-        frontmatter {
-          slug
-          title
-        }
-        html
+  query BlogPostTemplateQuery($slug: String!){
+    file(childMarkdownRemark: {frontmatter: {slug: {eq: $slug}}}) {
+    childMarkdownRemark {
+      frontmatter {
+        slug
+        title
       }
+      html
     }
+  }
   }
 `
