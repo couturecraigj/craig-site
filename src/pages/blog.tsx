56f6549e38990370f2785bcc8e03985302dbdf22
couturecraigj <couturecraigj@gmail.com>
2020-06-12 15:27:21 -0500

    fixing showing draft blog posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,12 +4,47 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
+const dev = process.env.NODE_ENV !== "production"
+
+const PostList = ({posts}) => {
+  return posts.edges.map(({ node }) => (
+    <div key={node.childMarkdownRemark.frontmatter.slug}>
+      <Link to={`/${node.childMarkdownRemark.frontmatter.slug}`}>
+        {node.childMarkdownRemark.frontmatter.title}
+      </Link>
+    </div>
+  ))
+}
+
 const Blog = () => {
   const data = useStaticQuery(graphql`
-    query {
-      posts: allFile(
+    query ProductionBlogQuery {
+      devPosts: allFile(
         limit: 10
         filter: { sourceInstanceName: { eq: "posts" } }
+        skip: 0
+      ) {
+        edges {
+          node {
+            name
+            childMarkdownRemark {
+              frontmatter {
+                title
+                slug
+              }
+              html
+              timeToRead
+              excerpt
+            }
+          }
+        }
+      }
+      posts: allFile(
+        limit: 10
+        filter: {
+          sourceInstanceName: { eq: "posts" }
+          childMarkdownRemark: { frontmatter: { draft: { eq: false } } }
+        }
         skip: 0
       ) {
         edges {
@@ -34,13 +69,7 @@ const Blog = () => {
     <Layout>
       <SEO title="Blog" />
       <h1>Hi from the blog page</h1>
-      {data.posts.edges.map(({ node }) => (
-        <div key={node.childMarkdownRemark.frontmatter.slug}>
-          <Link to={`/${node.childMarkdownRemark.frontmatter.slug}`}>
-            {node.childMarkdownRemark.frontmatter.title}
-          </Link>
-        </div>
-      ))}
+      <PostList posts={(dev ? data.devPosts : data.posts)} />
     </Layout>
   )
 }
